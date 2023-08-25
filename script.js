7931f773c570874cc76307cf57121009b01c8c1c
Cloneartist <73981868+cloneartist@users.noreply.github.com>
2023-08-25 23:37:50 +0530

    Add Passphrase Option, UI edits

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
-document.getElementById('generate').addEventListener('click', function () {
-    var length = document.getElementById('length').value;
-    var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-    var password = '';
+document.addEventListener('DOMContentLoaded', function() {
+    var body = document.body;
+    var darkModeToggle = document.getElementById('dark-mode-toggle');
 
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset[randomIndex];
+    darkModeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+    });
+
+    document.getElementById('generate').addEventListener('click', function () {
+        var length = document.getElementById('length').value;
+        var charset = document.getElementById('charset').value || 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+        var password = '';
+
+        if (document.getElementById('passphrase').checked) {
+            password = generatePassphrase();
+        } else {
+            for (var i = 0; i < length; i++) {
+                var randomIndex = Math.floor(Math.random() * charset.length);
+                password += charset[randomIndex];
+            }
+        }
+
+        document.getElementById('password').value = password;
+
+        var strength = evaluatePasswordStrength(password);
+        var strengthIndicator = document.getElementById('strength');
+        strengthIndicator.textContent = strength.charAt(0).toUpperCase() + strength.slice(1);
+        strengthIndicator.className = 'password-strength strength-' + strength; // Apply the strength class for styling
+        
+        generateQRCode(password); // Generate QR code
+    });
+
+    function generatePassphrase() {
+        var words = ['apple', 'banana', 'cherry', 'dog', 'elephant', 'flower', 'gorilla', 'happy', 'icecream', 'jelly'];
+        var passphrase = '';
+
+        for (var i = 0; i < 4; i++) {
+            var randomIndex = Math.floor(Math.random() * words.length);
+            passphrase += words[randomIndex];
+            if (i !== 3) {
+                passphrase += '-';
+            }
+        }
+
+        return passphrase;
     }
 
-    document.getElementById('password').value = password;
-    
-    var strength = evaluatePasswordStrength(password);
-    var strengthIndicator = document.getElementById('strength');
-    strengthIndicator.textContent = strength.charAt(0).toUpperCase() + strength.slice(1);
-    strengthIndicator.className = strength; // Apply the strength class for styling
-});
+    function evaluatePasswordStrength(password) {
+        var strength = 'weak';
 
-function evaluatePasswordStrength(password) {
-    var strength = 'weak';
+        if (password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*()_+]/.test(password)) {
+            strength = 'strong';
+        } else if (password.length >= 6) {
+            strength = 'medium';
+        }
 
-    if (password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*()_+]/.test(password)) {
-        strength = 'strong';
-    } else if (password.length >= 6) {
-        strength = 'medium';
+        return strength;
     }
 
-    return strength;
-}
+    document.getElementById('copy').addEventListener('click', function () {
+        var passwordField = document.getElementById('password');
+        
+        passwordField.select();
+        passwordField.setSelectionRange(0, 99999);
+        document.execCommand('copy');
+        passwordField.setSelectionRange(0, 0);
+    });
 
-document.getElementById('copy').addEventListener('click', function () {
-    var passwordField = document.getElementById('password');
-    
-    passwordField.select();
-    passwordField.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-    passwordField.setSelectionRange(0, 0);
+    function generateQRCode(password) {
+        var qr = new QRious({
+            element: document.getElementById('qrCanvas'),
+            size: 150,
+            value: password
+        });
+    }
 });
