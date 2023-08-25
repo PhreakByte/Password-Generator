368445ba58642bdb17017bf627ae0d2954811003
Cloneartist <73981868+cloneartist@users.noreply.github.com>
2023-08-25 23:18:32 +0530

    Password strength

--- script.js
+++ script.js
@@ -9,4 +9,30 @@ document.getElementById('generate').addEventListener('click', function () {
     }
 
     document.getElementById('password').value = password;
+    
+    var strength = evaluatePasswordStrength(password);
+    var strengthIndicator = document.getElementById('strength');
+    strengthIndicator.textContent = strength.charAt(0).toUpperCase() + strength.slice(1);
+    strengthIndicator.className = strength; // Apply the strength class for styling
+});
+
+function evaluatePasswordStrength(password) {
+    var strength = 'weak';
+
+    if (password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*()_+]/.test(password)) {
+        strength = 'strong';
+    } else if (password.length >= 6) {
+        strength = 'medium';
+    }
+
+    return strength;
+}
+
+document.getElementById('copy').addEventListener('click', function () {
+    var passwordField = document.getElementById('password');
+    
+    passwordField.select();
+    passwordField.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    passwordField.setSelectionRange(0, 0);
 });
